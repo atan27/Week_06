abc14a7bd31872a56915940e122325ed10c23a53
atan27 <atan27@illinois.edu>
2018-10-10 20:30:21 -0500

    added a list to home page and allowed for an element to be added on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 
 class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: "",
+      todos: [
+        "thing 1",
+        "thing 2",
+        "thing 3",
+        "thing 4",
+        "thing 5",
+        "thing 6"
+      ],
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: ""
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>About</h2>
+        {this.state.todos.map(todo => (
+          <li key={todo}> {todo}</li>
+        ))}
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}> + </button>
+        </form>
       </div>
     );
   }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,28 @@
-
-
 import React, { Component } from "react";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: "",
+      todos: [
+        "thing 1",
+        "thing 2",
+        "thing 3",
+        "thing 4",
+        "thing 5",
+        "thing 6"
+      ],
+    };
+  }
   render() {
     return ( <div>
         <h2>Home</h2>
+        {this.state.todos.map(todo => (
+          <li key={todo}>
+          {todo + " "}
+          </li>
+        ))}
       </div>);
   }
 }
